a4873fe121fd858a5aca1016873f2245a49931bd
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2023-06-19 10:41:54 +0200

    revert to using hardcoded package name provider

--- provider.go
+++ provider.go
@@ -9,6 +9,8 @@ import (
 	"github.com/grafana/thema"
 )
 
+const providerPackageName = "provider"
+
 // Provider is a structure that can hold a set of core, composable and
 // custom kinds together with provider metadata.
 type Provider struct {
@@ -53,7 +55,7 @@ func LoadProvider(fsys fs.FS, rt *thema.Runtime) (*Provider, error) {
 		rt = thema.NewRuntime(ctx)
 	}
 
-	bi, err := LoadInstance("", "", fsys)
+	bi, err := LoadInstance("", providerPackageName, fsys)
 	if err != nil || bi.Err != nil {
 		if err == nil {
 			err = bi.Err
--- testdata/provider/custom-kinds/cue.mod/module.cue
+++
@@ -1,1 +0,0 @@
-module: "github.com/grafana/kindsys/testdata/provider"
