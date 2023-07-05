ef64efb7f1fd23013292b89b2d00938db2f74ff5
sam boyer <sdboyer@grafana.com>
2023-07-05 15:52:03 -0400

    Remove resource subdir

--- resource/doc.go
+++
@@ -1,4 +0,0 @@
-// Package resource defines generic types for representing Grafana resources - instances of kindsys kinds.
-//
-// See the [Resource] docs for more information about how resources relate to [kindsys.Kind]s.
-package resource
